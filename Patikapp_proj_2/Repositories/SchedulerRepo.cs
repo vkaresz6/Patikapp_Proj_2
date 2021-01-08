33a3030c7a8d85f95005ead9db1f3040296a6284
Varga Károly <vkaresz6@gmail.com>
2021-01-08 18:14:07 +0100

    x

---
+++ Patikapp_proj_2/Repositories/SchedulerRepo.cs
@@ -0,0 +1,16 @@
+﻿using Patikapp_proj_2.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patikapp_proj_2.Repositories
+{
+    class SchedulerRepo
+    {
+
+
+
+    }
+}
